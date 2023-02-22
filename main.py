c519372dae854d33ce309b706b3755c074460cff
aysebat <aaysebatt@gmail.com>
2023-02-22 22:37:09 +0300

    ticket method implement

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import random
 import sqlite3
 import string
 
+from fpdf import FPDF
+
+
 class User:
 
     def __init__(self, name):
@@ -12,6 +15,9 @@ class User:
         if seat.is_free():
             if card.validate(price=seat.get_price()):
                 seat.occupy()
+                ticket = Ticket(user=self.name, price=seat.get_price(), seat_number=seat_id)
+                ticket.to_pdf()
+                return "Your payment successful"
 
             else:
                 return "There was a problem with your card"
@@ -96,7 +102,51 @@ class Ticket:
         self.seat_number = seat_number
 
     def to_pdf(self):
-        pass
+        "Create a pdf ticket"
+        pdf = FPDF(orientation='p', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family='Times', style='B', size=24)
+        pdf.cell(w=0, h=80,  txt="Your Digital Ticket", border=1, ln=1, align='C')
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt="Name: ", border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt="Price: ", border=1)
+        pdf.set_font(family='Times', style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.price), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family='Times', style='B', size=14)
+        pdf.cell(w=100, h=25, txt="Seat Number: ", border=1)
+        pdf.set_font(family='Times', style="", size=12)
+        pdf.cell(w=0, h=25, txt=str(self.seat_number), border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.output("sample.pdf", "F")
+
+
+if __name__ == "main":
+    name = input("Your Full Name: ")
+    seat_id = input("Preferred seat number: ")
+    card_type = input("Your card type: ")
+    card_number = input("Your card number: ")
+    card_cvc = input("Your card cvc: ")
+    card_holder  = input("Card holder name: ")
+
+
+    user = User(name=name)
+    seat = Seat(sea_id=seat_id)
+    card = Card(type=card_type,
+                number=card_number,
+                cvc=card_cvc,
+                holder=card_holder)
+
+    print(user.buy(seat=seat, card=card))
 
 
 
