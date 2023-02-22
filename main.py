8e9ac9db90fa2077ca06820d8e5a3eb15fdc3d78
aysebat <aaysebatt@gmail.com>
2023-02-22 21:52:46 +0300

    get_price method in Seat Class

--- main.py
+++ main.py
@@ -18,14 +18,22 @@ class Seat:
         self.seat_id = seat_id
 
     def get_price(self):
-        pass
+        """Get the price for a certain seat"""
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "price" FROM "Seat" WHERE "seat_id" = ?
+        """, [self.seat_id])
+        price = cursor.fetchall()[0][0]
+        return price
+
 
     def is_free(self):
         """Check in database if the seat taken or not"""
         connection = sqlite3.connect(self.database)
         cursor = connection.cursor()
         cursor.execute("""
-        SELECT "taken" from "Seat" WHERE "seat_id" = ?
+        SELECT "taken" FROM "Seat" WHERE "seat_id" = ?
         """, [self.seat_id])
         result = cursor.fetchall()[0][0]
         #0: indicate the seat is not taken so it is free
